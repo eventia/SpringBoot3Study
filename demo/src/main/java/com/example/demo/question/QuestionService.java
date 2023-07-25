e047ae2be23f99fe9e81029acbb6406e543281fd
eventia <eventia@gmail.com>
2023-07-25 17:34:58 +0900

    SpringBoot3 STUDY 2023-07-25-[17:34]

--- demo/src/main/java/com/example/demo/question/QuestionService.java
+++ demo/src/main/java/com/example/demo/question/QuestionService.java
@@ -1,6 +1,7 @@
 // 2-09 service 
 package com.example.demo.question;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +27,14 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+    
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 
 }
