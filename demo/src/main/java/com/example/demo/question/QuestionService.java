be2d4dda5521e75966f55da36dfad42e90c22554
eventia <eventia@gmail.com>
2023-07-25 16:48:52 +0900

    SpringBoot3 STUDY 2023-07-25-[16:48]

--- demo/src/main/java/com/example/demo/question/QuestionService.java
+++ demo/src/main/java/com/example/demo/question/QuestionService.java
@@ -2,8 +2,11 @@
 package com.example.demo.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.example.demo.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +17,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    
+    public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 
 }
