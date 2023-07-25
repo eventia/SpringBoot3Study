8617449462a96c3f2089efecd0f7afb9b7f8d75d
eventia <eventia@gmail.com>
2023-07-25 21:43:27 +0900

    SpringBoot3 STUDY 2023-07-25-[21:43]

--- demo/src/main/java/com/example/demo/question/QuestionRepository.java
+++ demo/src/main/java/com/example/demo/question/QuestionRepository.java
@@ -1,14 +1,17 @@
 package com.example.demo.question;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	
-	// QuestionRepository
 	Question findBySubject(String subject);
-	
-	// findBySubjectAndContent
 	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
 	
 
 }
