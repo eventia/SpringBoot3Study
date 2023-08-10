f018a11bb23eca5eeabaad3c86aeca9fd6e08760
eventia <eventia@gmail.com>
2023-08-10 21:41:39 +0900

    SpringBoot3 STUDY 2023-08-10-[21:41]

--- demo/src/main/java/com/example/demo/answer/AnswerService.java
+++ demo/src/main/java/com/example/demo/answer/AnswerService.java
@@ -1,9 +1,11 @@
 package com.example.demo.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.DataNotFoundException;
 import com.example.demo.question.Question;
 import com.example.demo.user.SiteUser;
 
@@ -14,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class AnswerService {
 	private final AnswerRepository answerRepository;
 	
-	 public Answer create(Question question, String content, SiteUser author) {
+	public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
@@ -22,5 +24,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+	 
+	public Answer getAnswer(Integer id) {
+	    Optional<Answer> answer = this.answerRepository.findById(id);
+	    if (answer.isPresent()) {
+	        return answer.get();
+	    } else {
+	        throw new DataNotFoundException("answer not found");
+	    }
+	}
+
+	public void modify(Answer answer, String content) {
+	    answer.setContent(content);
+	    answer.setModifyDate(LocalDateTime.now());
+	    this.answerRepository.save(answer);
+	}
+	
+	public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
--- demo/src/main/java/com/example/demo/question/QuestionService.java
+++ demo/src/main/java/com/example/demo/question/QuestionService.java
@@ -57,4 +57,7 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         this.questionRepository.save(question);
     }
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
 }
